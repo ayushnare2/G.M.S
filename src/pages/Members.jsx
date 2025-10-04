64854622a243ed0a6be6058717a78c2ef81a3de5
ayushnare2 <ayushnare@gmail.com>
2025-10-05 00:13:21 +0530

    fixed proj

--- src/pages/Members.jsx
+++ src/pages/Members.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Table, Form, Container } from 'react-bootstrap';
-import './members.css';
+import './Members.css';
 
 const Members = () => {
   const [members, setMembers] = useState([]);
